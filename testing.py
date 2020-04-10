cc9d336f97019fd27a1e50981c4836a6e837e54a
Daniel Higley <dhigley6@gmail.com>
2020-04-10 14:56:01 -0700

    Updated testing files.

--- testing.py
+++ testing.py
@@ -4,22 +4,25 @@ Module for testing new ideas
 
 import numpy as np
 import matplotlib.pyplot as plt 
-from sklearn.model_selection import GridSearchCV
+import tensorflow as tf
+from sklearn.metrics import mean_squared_error
 
-from pax_simulations import simulate_pax, model_photoemission, model_rixs, run_analyze_save_load
-from visualize import plot_result, summary, cv_plot
-import LRDeconvolve
+from pax_simulations import run_analyze_save_load
 
-DEFAULT_PARAMETERS = {
+PARAMETERS = {
     'energy_spacing': 0.005,
     'iterations': 1E2,
-    'simulations': 10,
-    'cv_fold': 2,
+    'simulations': 1E3,
+    'cv_fold': 3,
     'regularizer_widths': np.logspace(-3, -1, 10)
 }
 
-def run_test3(log10_num_electrons, rixs='schlappa', photoemission='ag'):
-    #run_analyze_save_load.run(log10_num_electrons, rixs, photoemission)
-    saved = run_analyze_save_load.load(log10_num_electrons, rixs, photoemission)
-    plot_result.make_plot(saved['deconvolver_gs'].best_estimator_, saved['sim'])
-    cv_plot.make_plot(saved['deconvolver_gs'])
+def run_test(log10_num_electrons=10, rixs='schlappa', photoemission='ag'):
+    run_analyze_save_load.run(log10_num_electrons, rixs, photoemission, **PARAMETERS)
+
+def tf_train_step(deconvolved, optimizer):
+    with tf.GradientTape() as tape:
+        reconvolved = tf.signal.convolve()
+        loss = mean_squared_error(measurement, reconvolved)
+    grads = tape.gradient(loss, deconvolved)
+    optimizer.apply_gradients(zip(deconvolved, last))
