7bea9c08b23ac0ef13d429861862b8b4a8d9b2a4
Daniel Higley <dhigley6@gmail.com>
2020-05-13 14:44:34 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
 
