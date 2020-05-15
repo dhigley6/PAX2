8435e76cd74703e15708b9bea9452e5dfeb5854a
Daniel Higley <dhigley6@gmail.com>
2020-05-14 18:50:59 -0700

    minor fix

--- pax_deconvolve/visualize/plot_result.py
+++ pax_deconvolve/visualize/plot_result.py
@@ -23,8 +23,8 @@ def _make_deconvolved_plot(ax, deconvolved):
     ax.plot(deconvolved.deconvolved_x, deconvolved.deconvolved_y_, label='Deconvolved')
 
 def _make_reconvolved_plot(ax, deconvolved):
-    ax.plot(deconvolved.reconvolved_x, deconvolved.measured_y_, label='Measured')
-    ax.plot(deconvolved.reconvolved_x, deconvolved.reconstruction_y_, label='Reconvolved')
+    ax.plot(deconvolved.reconstruction_x, deconvolved.measured_y_, label='Measured')
+    ax.plot(deconvolved.reconstruction_x, deconvolved.reconstruction_y_, label='Reconvolved')
 
 def _format_plot(axs):
     axs[0].set_xlabel('Photon Energy (eV)')
